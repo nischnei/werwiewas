37922e17711341c4f9eb7cad8d6c0122471a8e87
Nick Schneider <schneider.m.nick@gmail.com>
2024-12-16 15:00:27 +0100

    typing fixed and minor doc added

--- python/speechrecognition/README.md
+++ python/speechrecognition/README.md
@@ -1,5 +1,6 @@
 ```
 brew install ffmpeg
-brew install llvm
+brew install llvm@15
+export LLVM_CONFIG=/opt/homebrew/opt/llvm@15/bin/llvm-config
 poetry install
 ```
--- python/speechrecognition/speechrecognition/whisper.py
+++ python/speechrecognition/speechrecognition/whisper.py
@@ -1,11 +1,16 @@
 from pathlib import Path
+from typing import Optional
+
 import whisper
+# Working around to fix a whisper Future warning
+import functools
+whisper.torch.load = functools.partial(whisper.torch.load, weights_only=True)
 
 
 class WhisperRecognition:
-    def __init__(self, model_name="small"):
+    def __init__(self, model_name: str = "small"):
         self.model = whisper.load_model(model_name)
 
-    def transcribe(self, path: Path):
-        result = self.model.transcribe(path)
+    def transcribe(self, path: Path, initial_prompt: Optional[str]=None):
+        result = self.model.transcribe(path, initial_prompt=initial_prompt, fp16=False)
         return result["text"]
