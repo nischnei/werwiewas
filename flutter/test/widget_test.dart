7ddabbcc842907fb5ad769ff014357b4536bc1f7
Nick Schneider <schneider.m.nick@gmail.com>
2024-12-17 23:22:22 +0100

    fix: python and flutter tests fixed

--- flutter/test/widget_test.dart
+++ flutter/test/widget_test.dart
@@ -1,31 +1,17 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+// This is a basic Flutter widget test. More or less to test if all fires up correctly.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:www/app.dart';
 
-import 'package:www/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('App initial states as expected smoke test.',
+      (WidgetTester tester) async {
+    // Build the app and do nothing for now.
     await tester.pumpWidget(const WhoWhatWhereApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that all is setup
+    expect(find.text('Question:'), findsOneWidget);
+    expect(find.text('Answer:'), findsOneWidget);
+    expect(find.text('Homepage to query:'), findsOneWidget);
   });
 }
